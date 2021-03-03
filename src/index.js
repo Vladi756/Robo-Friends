8ededb7afa7e2db46df43c493bf199e8c1b96dc9
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-03-04 00:07:27 +0100

    index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App'
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import { robots } from './robots';
+
+ReactDOM.render(
+		<App />
+, document.getElementById('root'));
+
+reportWebVitals();
